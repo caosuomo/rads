2ead8acc38dbd067c7ffe1e45fad0c5cadaed337
Raf <rafael.frongillo@gmail.com>
2012-02-06 17:24:18 -0800

    commented first_return_times for uber readability

--- sandbox/first_return_times.py
+++ sandbox/first_return_times.py
@@ -1,18 +1,21 @@
 import networkx as nx
 
 def first_return_times(G,k):
-	d = dict()
+	return_times = dict()
+
+	# length = shortest path lengths <= k
+	# length[i][j] = length of shortest path i->j, if <= k
 	length = nx.all_pairs_shortest_path_length(G,k)
+	
 	for i in G.nodes_iter():
-		distances = [length[j][i]+1 for j in filter(lambda x: length[x].has_key(i),G.successors(i))]
-		print distances
+		# nodes = list of successors j which return to i
+		nodes = filter(lambda j: length[j].has_key(i),G.successors(i))
+		# distances for each successor j
+		distances = [length[j][i]+1 for j in ]
 		if distances:
-			d[i] = min(distances)
-			
-		# for j in G.successors_iter(i):
-		# 	if length[j].has_key(i) and length[j][i] + 1 < d[i]:
-		# 		d[i] = length[j][i] + 1
-	return d
+			return_times[i] = min(distances)
+
+	return return_times
 
 
 if __name__ == "__main__":
