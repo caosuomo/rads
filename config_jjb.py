18ebbeb60e555d3e32348b0ac8f6e1a75b6f3344
Jesse Berwald <jberwald@gmail.com>
2012-06-19 13:27:46 -0400

    Python doesn't seem to be able to load .dylib files on mac. And thinks .so are the wrong arch. But everything compiles without complaints…

--- config_jjb.py
+++ config_jjb.py
@@ -1,6 +1,6 @@
 
 exe = {
-	'cython': 'cython',
+	'cython': '/Applications/sage/local/bin/sage-cython', #cython
 	'c++': 'g++',
 	'c': 'gcc',
 	}
@@ -15,21 +15,22 @@ dirs = {
 include = {
 	'sage': '/Applications/sage/devel/sage-main/',
 	'sage c': '/Applications/sage/devel/sage-main/c_lib/include/',
-	'python': '/Library/Frameworks/Python.framework/Versions/Current/include/python2.7',
-	'cython': '/Library/Frameworks/Python.framework/Versions/Current/lib/python2.7/site-packages/Cython/Includes/',
-	'numpy': '/Library/Frameworks/Python.framework/Versions/Current/lib/python2.7/site-packages/numpy/core/include/',
+	'python': '/Library/Frameworks/EPD64.framework/Versions/7.3/include/python2.7/',
+	'cython': '/Library/Frameworks/EPD64.framework/Versions/7.3/lib/python2.7/site-packages/Cython/Includes/',
+	'numpy': '/Library/Frameworks/EPD64.framework/Versions/7.3/lib/python2.7/site-packages/numpy/core/include/',
 	'capd': '/Users/jberwald/src/capd/capdAlg/include/'
         }
 
 link = {
 	'capd': dirs['capd'],
-	'c++ cython': '-o'.split(), #'-llr'
-	'c++': '-lprim -o'.split() #-llr
+	'c++ cython': '',#.split(), #'-llr'
+	'c++': '-lprim'.split() #-llr
         }
 
 flags = {
-	'c': '-arch x86_64 -arch i386 -pthread -fno-strict-aliasing -DNDEBUG -g -fwrapv -O2 -Wall -fPIC'.split(), # -O2
-	'c++ cython': '-fPIC -dynamiclib -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-compatibility_version,1.0,-current_version,1.0,-install_name,'.split(),
+	'c': '-fno-strict-aliasing -fno-common -dynamic -arch x86_64 -DNDEBUG -g -O3'.split(),
+	# 'c': '-arch x86_64 -pthread -fno-strict-aliasing -DNDEBUG -g -fwrapv -O2 -Wall -fPIC'.split(), # -O2
+	'c++ cython': '-arch x86_64 -shared -fPIC,-dynamiclib -Wl,-headerpad_max_install_names,-undefined,dynamic_lookup,-compatibility_version,1.0,-current_version,1.0'.split(),
 	#'-pthread -shared -Wl,-Wl,-Bsymbolic-functions'.split(),
 	'c++': ''#'-D__USE_FILIB__ -lprim'.split()
         }
