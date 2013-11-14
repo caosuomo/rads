2597fb6883cce10878414743fe72d60e438cfc65
Jesse Berwald <jberwald@gmail.com>
2013-11-14 16:31:46 -0600

    refactored point.h to play well with C-XSC lib

--- src/cpp/point.h
+++ src/cpp/point.h
@@ -4,9 +4,8 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
-#include "rinterval.hpp"
-/* #include "capd/capdAlglib.h" */
-/* #include "capd/intervals/Interval.h" */
+#include "interval.hpp"  // predefined interval arithmetic
+#include "ivector.hpp"
 
 using namespace cxsc;
 using namespace std;
@@ -14,23 +13,17 @@ using namespace std;
 template < class T >
 class PointBase 
 {
-    // vector of arbitrary length. 
-    // below v( dim ) will initialize a n-dim vector of type T  
-    // This line defines a vector of type T of arbitrary length.
-    //typedef capd::vectalg::Vector < T, 0 > DVector;
-  
-  typedef ivector < T, 0 > DVector; 
-
+  typedef ivector DVector; 
 
   public:
-    DVector v;
-    PointBase() {};
-    PointBase(int size) : v ( size ) {};
-    PointBase(int size, const T &t) : v(size,t) {};
-    T& operator[] (int i) { return v[i]; };
-    void set (int i, const T& t) { v[i]=t; };
-    const T& operator[] (int i) const { return v[i]; };
-    int size() const { return v.dimension(); };
+  DVector v;
+  PointBase() {};
+  PointBase(int size) : v ( size ) {};
+  PointBase(int size, const T &t) : v(size,t) {};
+  T& operator[] (int i) { return v[i]; };
+  void set (int i, const T& t) { v[i]=t; };
+  const T& operator[] (int i) const { return v[i]; };
+  int size() const { return VecLen( v ); }; // C-XSC version of dimension
     
     friend ostream& operator<< (ostream &out, const PointBase<T> &p)
     {
@@ -39,12 +32,12 @@ class PointBase
     };
 };
 
-typedef PointBase < double > Point;
+typedef interval DInterval;
 typedef PointBase< interval > IPoint;
 //typedef capd::intervals::Interval< double > DInterval;
 //typedef PointBase< capd::intervals::Interval< double > > IPoint;
 
-#include "treeutil.h"			// for the vector operator<< 
+//#include "treeutil.h"			// for the vector operator<< 
 #endif
 
 /////////
@@ -58,7 +51,7 @@ int main()
     int i;
     cout << "enter a number: ";
     cin >> y;
-    cout << "enter an integer: ";
+    cout << "enter an integer dimension >= 2: ";
     cin >> i;
     cout << "you entered " << y << " and " << i << endl;
 
@@ -67,16 +60,25 @@ int main()
 
     IPoint P ( i );
 
-    cout << "P.v is " << i << " dimensional: P.v = " << P.v << endl;
+    cout << "P.v is a " << P.size() 
+	 << " dimensional interval vector and is intialized as:\n P.v =\n" 
+	 << P.v << endl;
 
     // fill P.v[0]
     DInterval x0[] = { DInterval( 1., 2. ) };
     DInterval x1( 3., 4. );
 
-    P.v[0] = x0[0];
-    P.v[1] = x1;
+    interval a,b,c;
+    a = y;
+    b = 3.14;
+    c = 1.1;
+    
+    P.v[1] = a;
+    P.v[2] = b;
+    P.set(i,c);
 
-    cout << "Set entries in P ==> P.v = " << P.v << endl;
+    cout << "Set entries in P ==> P.v =\n" << P.v << endl;
+    cout << "(The first and last are hardcoded. See the code.)" << endl;
     
     return 0;
 
