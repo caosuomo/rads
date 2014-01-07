5919bdd0dc13706d840e1d8fa1c77689cbd02d28
Jesse Berwald <jberwald@gmail.com>
2014-01-07 00:32:59 -0600

    switched from ivector to standard vector

--- src/cpp/point.h
+++ src/cpp/point.h
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <vector>
 #include "interval.hpp"  // predefined interval arithmetic
-#include "ivector.hpp"
+//#include "ivector.hpp"
 
 using namespace cxsc;
 using namespace std;
@@ -16,15 +16,17 @@ class PointBase
   // typedef ivector DVector; 
 
  public:
-  ivector v;
+  //ivector v;
+  vector< T > v;
   PointBase() {};
   PointBase(int size) : v ( size ) {};
   PointBase(int size, const T &t) : v(size,t) {};
   T& operator[] (int i) { return v[i]; };
   void set (int i, const T& t) { v[i]=t; };
   const T& operator[] (int i) const { return v[i]; };
-  int size() const { return VecLen( v ); }; // C-XSC version of dimension
-    
+  //int size() const { return VecLen( v ); }; // C-XSC version of dimension
+  int size() const { return v.size(); };
+
     friend ostream& operator<< (ostream &out, const PointBase<T> &p)
     {
       out << p.v;
@@ -32,6 +34,8 @@ class PointBase
     };
 };
 
+//typedef PointBase< interval > Point;
+typedef PointBase< double > Point;
 typedef PointBase< interval > IPoint;
 
 #include "treeutil.h"			// for the vector operator<< 
